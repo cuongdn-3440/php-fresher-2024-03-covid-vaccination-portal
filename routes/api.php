11b50a4ec7b8c82abc594d39fe5347999277d6a5
cuongdn-3440 <dang.nghiep.cuong@sun-asterisk.com>
2024-05-08 11:12:27 +0700

    fix_feature/show_notification_dot_on_UI
    feature/admin_dashboard
    
    rm console.log(), redundant file
    
    fix_test/controller_unit_test_branch_coverage
    
    fix coding style
    
    fix coding style

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\V1\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,9 +20,30 @@ Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'current
 // api/v1
 Route::middleware('auth:sanctum')
     ->group(function () {
-        Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
-            Route::apiResource('businesses', BusinessController::class);
-            Route::apiResource('schedules', ScheduleController::class);
-            Route::apiResource('notifications', NotificationController::class);
-        });
+        Route::prefix('v1')
+            ->namespace('App\Http\Controllers\Api\V1')
+            ->group(function () {
+                Route::apiResource('businesses', BusinessController::class);
+                Route::apiResource('schedules', ScheduleController::class);
+                Route::prefix('notifications')
+                    ->controller('NotificationController')
+                    ->group(function () {
+                        Route::get('', 'index');
+                        Route::get('countUnread', 'countUnread');
+                        Route::post('markAllAsRead', 'markAllAsRead');
+                    });
+
+                Route::prefix('dashboard')
+                    ->controller(DashboardController::class)
+                    ->group(function () {
+                        Route::get('totalDoses', 'getTotalDoses')
+                        ->name('dashboard.total_dose');
+                        Route::get('totalPeopleWithOnlyFirstDose', 'getTotalPeopleWithOnlyFirstDose')
+                        ->name('dashboard.total_people_with_only_first_dose');
+                        Route::get('totalPeopleWithOverOneDose', 'getTotalPeopleWithOverOneDose')
+                        ->name('dashboard.total_people_with_over_one_dose');
+                        Route::get('totalPeopleWithNoDose', 'getTotalPeopleWithNoDose')
+                        ->name('dashboard.total_people_with_no_dose');
+                    });
+            });
     });
